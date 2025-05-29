62653273829f35ba8436b993eba07e75368ac956
Matias <matias.carvajald@usm.cl>
2025-05-29 00:21:07 -0400

    Ola

--- database/migrations/2025_05_28_195413_create_productos_table.php
+++ database/migrations/2025_05_28_195413_create_productos_table.php
@@ -9,24 +9,23 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::create('productos', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-            $table->string('producto')->nullable(); 
-            $table->string('nombre');
-            $table->text('descripcion')->nullable();
-            $table->integer('cantidad')->default(0);
-            $table->string('categoria')->nullable();
-            $table->string('Numero_Serie')->unique()->nullable();
-            $table->string('Modelo')->nullable();
-            $table->string('Marca')->nullable();
-            $table->string("Numero_Inventario")->unique()->nullable();
-            $table->string('Ubicacion')->nullable();
-            $table->string('Estado')->default('Disponible');
-        });
-    }
+    Schema::create('productos', function (Blueprint $table) {
+        $table->id();
+        $table->timestamps();
+        $table->string('producto')->nullable();
+        $table->string('nombre');
+        $table->text('descripcion')->nullable();
+        $table->integer('cantidad')->default(0);
+        
+        $table->foreignId('categoria_id')->nullable()->constrained('categorias')->nullOnDelete();
+        $table->foreignId('marca_id')->nullable()->constrained('marcas')->nullOnDelete();
+        
+        $table->string('Numero_Serie')->unique()->nullable();
+        $table->string('Modelo')->nullable();
+        $table->string("Numero_Inventario")->unique()->nullable();
+        $table->string('Ubicacion')->nullable();
+        $table->string('Estado')->default('Disponible');
+    });
 
     /**
      * Reverse the migrations.
